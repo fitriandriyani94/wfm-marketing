3fa95aa37e203c68cb7d6e7c9bb7ba8912855909
unknown <fitriandriyani94>
2019-09-09 15:33:24 +0700

    Updates

---
+++ precache-manifest.508f084a8babf358b54af50f7322a33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56056b025cde3b98bc7839b2d6a11281",
+    "url": "/wfm-marketing/index.html"
+  },
+  {
+    "revision": "01577fbffd9e7a6c5485",
+    "url": "/wfm-marketing/static/css/2.ee615ac2.chunk.css"
+  },
+  {
+    "revision": "e5ee886bfee4354c34fa",
+    "url": "/wfm-marketing/static/css/main.21e59563.chunk.css"
+  },
+  {
+    "revision": "01577fbffd9e7a6c5485",
+    "url": "/wfm-marketing/static/js/2.7e424307.chunk.js"
+  },
+  {
+    "revision": "e5ee886bfee4354c34fa",
+    "url": "/wfm-marketing/static/js/main.6456984c.chunk.js"
+  },
+  {
+    "revision": "bd94ade94de3b3bae879",
+    "url": "/wfm-marketing/static/js/runtime~main.802ca80a.js"
+  }
+]);
